2337f44faf35e8901fac28398cbcf967ffac4c12
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-02-10 20:08:03 +0700

    Membuat single post route

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('container')
+    <article class='mb-5'>
+        <h2>{{ $post['title'] }}</a></h2>
+        <h5>By: {{ $post['author'] }}</h5>
+        <p>{{ $post['body'] }}</p>
+    </article>
+
+    <a href="/blog">Back to Posts</a>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Halaman Posts</h1>
+    @foreach ($posts as $post)
+        <article class='mb-5'>
+            <h2>
+                <a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a>
+            </h2>
+            <h5>By: {{ $post['author'] }}</h5>
+            <p>{{ $post['body'] }}</p>
+        </article>
+    @endforeach
 @endsection
