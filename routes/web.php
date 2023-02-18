47f9ede3b039f097fbb1ec49b232504816397210
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-02-18 10:52:16 +0700

    Menampilkan post berdasarkan author (user_id)

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\Post;
 use App\Http\Controllers\PostController;
 
 use App\Models\Category;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,5 +47,12 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name,
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => 'User Posts',
+        'posts' => $author->posts,
     ]);
 });
