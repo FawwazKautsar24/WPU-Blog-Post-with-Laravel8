ac1e595654d51f9b206ddb34db4400e3516a99f1
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-02-22 00:51:02 +0700

    Membuat hero & card untuk post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,23 @@
 @extends('layouts.main')
 
 @section('container')
-    <article class='mb-5'>
-        <h2>{{ $post->title }}</a></h2>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <article>
+                    <h2 class="mb-3">{{ $post->title }}</a></h2>
+            
+                    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-        
-        {!! $post->body !!}
-    </article>
-
-    <a href="/posts">Back to Posts</a>
+                    <img src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" class="img-fluid" alt="{{ $post->category->name }}">
+                    
+                    <article class="my-3 fs-5">
+                        {!! $post->body !!}
+                    </article>
+                
+                    <a href="/posts" class="d-block mt-3">Back to Posts</a>
+                </article>
+            </div>
+        </div>
+    </div>
 @endsection
