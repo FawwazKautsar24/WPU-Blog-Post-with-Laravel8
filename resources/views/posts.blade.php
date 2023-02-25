21a00ea0b878d0956a1941a11a8fcce362b61578
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-02-25 14:12:05 +0700

    Membuat search form & seleksi query pada controller

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{ $title }}</h1>
+    <h1 class="text-center mb-3">{{ $title }}</h1>
+    
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/posts">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search . . ." name="search" value="{{ request('search') }}">
+                    <button class="btn btn-danger" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
     
     @if ($posts->count())
         <div class="card mb-3">
@@ -21,26 +32,26 @@
                 <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
             </div>
         </div>
-    @else
-        <p class="text-center fs-4">No post found.</p>
-    @endif
-
-    <div class="container">
-        <div class="row mb-3">
+    
+        <div class="container">
+            <div class="row mb-3">
                 @foreach ($posts->skip(1) as $post)
-                <div class="col-md-4 mb-3">
-                    <div class="card">
-                    <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
-                        <img src="https://source.unsplash.com/500x400/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title"><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h5>
-                            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}</a></p>
-                            <p class="card-text">{{ $post->excerpt }}</p>
-                            <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
+                            <img src="https://source.unsplash.com/500x400/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h5>
+                                <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}</a></p>
+                                <p class="card-text">{{ $post->excerpt }}</p>
+                                <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                            </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
             </div>
         </div>
+    @else
+        <p class="text-center fs-4">No post found.</p>
+    @endif
 @endsection
