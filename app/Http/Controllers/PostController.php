6b22f9698450fc6fe00d0f50e58284ebeea482fa
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-02-28 06:04:12 +0700

    Memperinformatif judul Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Category;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $title = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+        }
+        
         return view('posts', [
-            "title" => "All Posts",
+            "title" => "All Posts" . $title,
             'active' => 'posts',
             // "posts" => Post::latest()->filter()->get(),
             "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
