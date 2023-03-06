0de43dc72a5ea1392328b6395c8b314a3138c5a6
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-03-06 17:52:35 +0700

    Menambahkan data user ke DB pada register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -16,13 +18,21 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|max:255',
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:255'
         ]);
 
-        dd('Registrasi Berhasil!');
+        // dd('Registrasi Berhasil!');
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+        
+        // $request->session()->flash('success', 'Registration Successfull! Please login');
+        
+        User::create($validatedData);
+
+        return redirect('/login')->with('success', 'Registration Successfull! Please login');
     }
 }
