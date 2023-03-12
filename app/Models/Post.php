bc5c6f692d9acd2413c537b5e7298448eedcd5aa
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-03-12 17:54:34 +0700

    Membuat detail post for creator view

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -53,4 +53,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+    
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
