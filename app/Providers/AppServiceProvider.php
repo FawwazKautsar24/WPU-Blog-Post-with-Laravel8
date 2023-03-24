b4d699fff37b7c50ceb9623325de24574475fad9
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-03-24 19:22:52 +0700

    Menyipsikan/migrasi atribut baru (add_is_admin_to_users_table)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
 
         Gate::define('admin', function (User $user) {
-            return $user->username === 'faw';
+            return $user->is_admin;
         });
     }
 }
